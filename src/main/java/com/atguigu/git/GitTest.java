6f3f53936d234a39c6d0beac7d188a91744ea0c7
wkx5028039 <84769980+wkx5028039@users.noreply.github.com>
2022-11-27 22:52:44 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello exeed");
         System.out.println("111");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
